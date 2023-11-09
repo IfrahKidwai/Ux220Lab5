e25db428691900e2a5b0b5930be531816d5f8533
diy-pwa <ikidwai@diy-pwa.dev>
2023-11-08 21:17:41 -0500

    add custom elements

--- customelements.js
+++ customelements.js
@@ -1,7 +1,33 @@
 class IfrahElement extends HTMLElement{
     connectedCallback(){
     this.innerHTML = "Ifrah was here";
+    
+   }
+}
+customElements.define("x-ifrah", IfrahElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =`<div style="text-align:center">${this.innerHTML}</div>`;
    }
 }
 
-customElements.define("x-ifrah", IfrahElement);
+customElements.define("x-center", CenterElement);
+
+//year
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+   }
+}
+
+customElements.define("x-fullyear", YearElement);
+
+//custom element
+class VioletElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML =`<div style="color:violet;">${this.innerHTML}</div>`;
+   }
+}
+
+customElements.define("x-violet", VioletElement);
